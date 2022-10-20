d7e50b8f8b277528a79af5257890eb4c29c4850f
Christopher Marinica <chris.marinica@gmail.com>
2022-10-20 12:49:45 -0400

    commit tree 36c28dab2463b86c65c5e9b687bca9c1e1322284

---
+++ index.js
@@ -0,0 +1,38 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function divide() {
+    // we'll fill this in shortly
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  function increment(n) {
+    return (n += 1);
+  }
+  function decrement(n) {
+    return (n -= 1);
+}
+function makeInt(x, base = 10) {
+    const parsed = parseInt(x, base = 10);
+    return parsed;
+  }
+  function preserveDecimal(n) {
+    const parsed = parseFloat(n);
+    return parsed;
+  }
